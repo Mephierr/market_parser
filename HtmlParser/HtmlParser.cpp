b8aa3af86b25992359ddddb9a46c38c6ddb27d07
Mephierr <error.9g@yandex.ru>
2024-11-22 21:43:35 +0300

    HtmlParser redid

--- HtmlParser/HtmlParser.cpp
+++ HtmlParser/HtmlParser.cpp
@@ -1,32 +1,60 @@
 #include<iostream>
 #include <curl/curl.h>
 #include <gumbo.h>
+#include <string>
 #include"HtmlParser.hpp"
+#include <vector>
 
 class HtmlParser {
 public:
-    std::string fetchPage(const std::string &url) {
-        // Используем libcurl для получения страницы
-        CURL *curl;
-        curl_global_init(CURL_GLOBAL_DEFAULT);
-        curl = curl_easy_init();
-        std::string readBuffer;
+    HtmlParser(const std::string& url) : url(url) {}
 
-        if(curl) {
+    bool fetch() {
+        CURL* curl;
+        CURLcode res;
+        curl = curl_easy_init();
+        if (curl) {
             curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, nullptr);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-            curl_easy_perform(curl);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &htmlContent);
+            res = curl_easy_perform(curl);
             curl_easy_cleanup(curl);
+            return res == CURLE_OK;
         }
-        curl_global_cleanup();
-        return readBuffer;
+        return false;
     }
 
-    void parseHtml(const std::string &html) {
-        // Используем Gumbo для парсинга
-        GumboOutput* output = gumbo_parse(html.c_str());
+    void parse() {
+        GumboOutput* output = gumbo_parse(htmlContent.c_str());
+        extractLinks(output->root);
         gumbo_destroy_output(&kGumboDefaultOptions, output);
-        std::cout << "HTML parsed." << std::endl;
+    }
+ 
+    void printLinks() const {
+        for (const auto& link : links) {
+            std::cout << link << std::endl;
+        }
+    }
+
+private:
+    std::string url;
+    std::string htmlContent;
+    std::vector<std::string> links;
+
+    static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+        ((std::string*)userp)->append((char*)contents, size * nmemb);
+        return size * nmemb;
+    }
+
+    void extractLinks(GumboNode* node) {
+        if (node->type == GUMBO_NODE_ELEMENT && node->v.element.tag == GUMBO_TAG_A) {
+            GumboAttribute* href = gumbo_get_attribute(&node->v.element.attributes, "href");
+            if (href) {
+                links.push_back(href->value);
+            }
+        }
+        for (size_t i = 0; i < node->v.element.children.length; ++i) {
+            extractLinks(static_cast<GumboNode*>(node->v.element.children.data[i]));
+        }
     }
 };
--- HtmlParser/HtmlParser.hpp
+++ HtmlParser/HtmlParser.hpp
@@ -6,9 +6,13 @@
 
 class HtmlParser {
 public:
-    std::string fetchPage(const std::string &url) ;
-
-    void parseHtml(const std::string &html) ;
+    HtmlParser(const std::string& url);
+    bool fetch();
+    void parse();
+    void printLinks();
+private:
+    static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
+    void extractLinks(GumboNode* node);
     
 };
 
